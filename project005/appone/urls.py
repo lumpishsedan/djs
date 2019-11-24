7e21f1dcef2f3b9fe62def3690129619fc368caa
lumpishsedan <58128806+lumpishsedan@users.noreply.github.com>
2019-11-24 15:30:10 +0530

    completed - query listview

--- project005/appone/urls.py
+++ project005/appone/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .views import HomePageView, SelectView, PostView, PostDetailView
 urlpatterns = [
-   path('', HomePageView.as_view(), name='home'),
+   path('', HomePageView, name='home'),
    path('select/', SelectView.as_view(), name='select'),
    path('post/new/', PostView.as_view(), name='post_new'), # new
    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
--- project005/appone/views.py
+++ project005/appone/views.py
@@ -2,10 +2,11 @@
 from django.views.generic import TemplateView, DetailView, ListView # new
 from django.views.generic.edit import CreateView # new
 
+from django.shortcuts import render
+
 from .models import Post
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+
 class SelectView(TemplateView):
     template_name = 'select.html'
 
@@ -18,7 +19,13 @@ class PostDetailView(DetailView): # new
     model = Post
     template_name = 'post_detail.html'
 
-class HomePageView(ListView):
+""" class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
-    context_object_name = 'all_posts_list' # new
+    context_object_name = 'all_posts_list' # new """
+
+def HomePageView(request):
+    key2select = Post.objects.filter(keyone=2)
+    return render(request, 'home.html', {
+        'key2select': key2select,
+    })
